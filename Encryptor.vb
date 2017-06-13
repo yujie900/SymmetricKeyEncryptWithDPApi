efa5b76726650cbc11ff79672af050af973f1f10
yujie900 <yujie@clear-measure.com>
2017-06-13 17:00:38 -0500

    initial commit

---
+++ Encryptor.vb
@@ -0,0 +1,50 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Encryptor
+    Public Function Encrypt(valueToEncrypt As String, handle As String) As Integer
+        Dim toEncrypt As Byte() = Encoding.ASCII.GetBytes(valueToEncrypt)
+        Dim entropy(15) As Byte
+        Dim provider As New RNGCryptoServiceProvider()
+        provider.GetBytes(entropy)
+        Dim fStream As New FileStream(handle, FileMode.OpenOrCreate)
+        Dim bytesWritten As Integer = EncryptDataToStream(toEncrypt, entropy, DataProtectionScope.LocalMachine, fStream)
+        fStream.Close()
+
+        Return bytesWritten
+    End Function
+
+    Private Function EncryptDataToStream(buffer() As Byte, entropy() As Byte, scope As DataProtectionScope, s As Stream) _
+        As Integer
+        If buffer.Length <= 0 Then
+            Throw New ArgumentException("Buffer")
+        End If
+        If buffer Is Nothing Then
+            Throw New ArgumentNullException("buffer")
+        End If
+        If Entropy.Length <= 0 Then
+            Throw New ArgumentException("Entropy")
+        End If
+        If Entropy Is Nothing Then
+            Throw New ArgumentNullException("entropy")
+        End If
+        If S Is Nothing Then
+            Throw New ArgumentNullException("s")
+        End If
+        Dim length As Integer = 0
+
+        ' Encrypt the data in memory. The result is stored in the same same array as the original data.
+        Dim encrptedData As Byte() = ProtectedData.Protect(Buffer, Entropy, Scope)
+
+        ' Write the encrypted data to a stream.
+        If S.CanWrite AndAlso Not (encrptedData Is Nothing) Then
+            S.Write(encrptedData, 0, encrptedData.Length)
+
+            length = encrptedData.Length
+        End If
+
+        ' Return the length that was written to the stream. 
+        Return length
+    End Function 'EncryptDataToStream
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SymmetricEncryptionWithDPApi")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SymmetricEncryptionWithDPApi")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c53adb38-9f4d-4284-99af-45a229da3e11")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
